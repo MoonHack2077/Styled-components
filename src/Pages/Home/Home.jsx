22175126c9d86d660835922972175410e57a7730
Juanma <arenasrinconj@gmail.com>
2022-04-20 23:47:16 -0500

    background color, and 'structure' proposed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Main } from './Home.style.js';
+import React , { useState , useEffect } from 'react';
+import { Main , City , Others } from './Home.style.js';
+
+import { locationSearch } from '../../Services/fetches.js';
 
 function Home(){
+    const [ city , setCity ] = useState({});
+    const london = 'london';
+
+    const fetched = () =>{
+        locationSearch(london)
+        .then(setCity)
+        .catch(console.log)
+    }
+    useEffect(()=>{
+        fetched();
+    },[])
     return(
         <Main>
-            
+            <City>
+                <ul>
+                    <li>{city}</li>
+                    <li>{city}</li>
+                </ul>
+            </City>
+            <Others>
+                XDD
+            </Others>
         </Main>
     );
 }
--- src/Pages/Home/Home.style.js
+++ src/Pages/Home/Home.style.js
@@ -1,8 +1,29 @@
 import styled from 'styled-components';
-import { backgroundColor } from '../../constants.js';
+import { primaryColor , backgroundColor, textColor } from '../../constants.js';
+
+//MOBILE FIRST
 
 export const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* justify-content: center; */
     width: 100vw;
     height: 100vh;
-    background-color: ${ backgroundColor }
+    background-color: ${ backgroundColor };
+    color: ${ textColor }
 `;
+
+export const City = styled.aside`
+    width: 100%;
+    height: 50%;
+    background-color: ${ primaryColor }
+`
+
+export const Others = styled.section`
+    width: 100%;
+    outline: 1px solid red;
+    /* height: 100%; */
+    margin: 100px;
+    background-color: #000
+`
